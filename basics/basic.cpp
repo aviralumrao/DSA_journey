8ad9cb26de871bfb1e8e8a680b26d98178ff6990
aviralumrao <aviral0419@gmail.com>
2026-01-23 20:28:40 +0530

    understanding printin and input

---
+++ basics/basic.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hey Aviral!\n";
+
+    // int x, y;
+    // cin >> x >> y;
+
+    // cout << "Value of x: " << x << " and y: " << y <<"\n";
+
+    string str;
+    cout << "Input the string: ";
+    getline(cin, str);
+
+    cout << str;
+    return 0;
+}
