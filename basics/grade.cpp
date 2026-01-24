52f189c6087fe21288eb6ba0565937c9a45a7ecc
aviralumrao <aviral0419@gmail.com>
2026-01-24 21:38:28 +0530

    Print grade according to marks

--- basics/grade.cpp
+++ basics/grade.cpp
@@ -7,8 +7,29 @@ e. 60 to 79 - B
 f. 80 to 100 - A
 Ask user to enter marks and print the corresponding grade.*/
 
-#include<iostream>
-void main()
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
 {
-    cout
+    int marks;
+    cout << "Input the marks : ";
+    cin >> marks;
+
+    if (marks < 25)
+        cout << "F";
+    else if (marks >= 25 && marks <= 44)
+        cout << "E";
+    else if (marks >= 45 && marks <= 49)
+        cout << "D";
+    else if (marks >= 50 && marks <= 59)
+        cout << "C";
+    else if (marks >= 60 && marks <= 79)
+        cout << "B";
+    else if (marks >= 80 && marks <= 100)
+        cout << "A";
+    else
+        cout << "INVALID INPUT";
+
+    return 0;
 }
