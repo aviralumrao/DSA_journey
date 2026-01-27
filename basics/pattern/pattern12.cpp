8fbcfa36690963f8bfe0af1663963a0bc060bb45
aviralumrao <aviral0419@gmail.com>
2026-01-27 15:30:43 +0530

    Number Crown Pattern

---
+++ basics/pattern/pattern12.cpp
@@ -0,0 +1,29 @@
+// Pattern - 11: Binary Number Triangle Pattern
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int n)
+{
+    int i,j,k;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            cout<< j;
+        }
+        for(j=1;j<=2*n-2*i;j++){
+            cout <<" ";
+        }
+        for(j=i;j>=1;j--){
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cout << "Input an Integer N = ";
+    cin >> t;
+    print(t);
+    return 0;
+}
