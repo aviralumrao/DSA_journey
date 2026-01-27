e96e6e6565cbdc8f631859f0fcf2e5d76c58983f
aviralumrao <aviral0419@gmail.com>
2026-01-27 15:10:30 +0530

    Right-Angled Triangle Pattern

--- basics/pattern/pattern2.cpp
+++ basics/pattern/pattern2.cpp
@@ -26,32 +26,3 @@ int main()
     print(t);
     return 0;
 }
-/* Pattern-2: Right-Angled Triangle Pattern
-    Given an integer N, print the following pattern : 
-    *
-    **
-    ***
-    ****
-*/
-
-#include <bits/stdc++.h>
-using namespace std;
-
-void print(int n){
-    for (int i=1; i<=n; i++){
-        for(int j=1; j<=i; j++){
-            cout << "* ";
-        }
-        cout << endl;
-    }
-}
-
-
-int main()
-{
-    int t;
-    cout << "Input an Integer N = ";
-    cin >> t;
-    print(t);
-    return 0;
-}
