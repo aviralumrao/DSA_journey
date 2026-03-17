46ec81e1b03b1cfb0ec7cf6e30d1dca8657b24db
aviralumrao <aviral0419@gmail.com>
2026-03-17 23:23:50 +0530

    add: implement bubble sort algorithm

---
+++ 02_sorting/03_insertion_sort.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int arr[], int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j])
+        {
+            swap(arr[j-1],arr[j]);
+            j--;
+        }
+    }
+
+    // Print the sorted array
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    bubble_sort(arr, n);
+
+    return 0;
+}
